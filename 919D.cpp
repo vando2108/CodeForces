c8258718247a9411bb445dc32eeaee23ee036673
vando2108 <35914996+vando2108@users.noreply.github.com>
2018-01-31 22:37:27 +0700

    Create 919D.cpp

---
+++ 919D.cpp
@@ -0,0 +1,120 @@
+//KP
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//Scan
+#define sf(n)       scanf("%d", &n)
+#define sff(a,b)    scanf("%d %d", &a, &b)
+#define sfff(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define sl(n)       scanf("%lld", &n)
+#define sll(a,b)    scanf("%lld %lld", &a, &b)
+#define slll(a,b,c) scanf("%lld %lld %lld", &a, &b, &c)
+
+// Loop
+#define FOR(i,a,b) for (int i = a; i <= b; i++)
+#define FORD(i,a,b) for (int i = a; i >= b; i--)
+#define rep(i,a,b) for (int i = a; i < b; i++)
+#define repd(i,a,b) for (int i = a - 1; i >= b; i--)
+#define FORall(it, a) for (__typeof((a).begin()) it = (a).begin(); it != (a).end(); it++)
+
+// Define
+#define ms(s, n) memset(s, n, sizeof(s))
+#define sz(a) int((a).size())
+#define present(t, x) (t.find(x) != t.end())
+#define all(a) (a).begin(), (a).end()
+#define uni(a) (a).erase(unipq(all(a)), (a).end())
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define fi first
+#define se second
+#define prec(n) fixed<<setprecision(n)
+#define fbit(n, i) (((n) >> (i)) & 1)
+#define bitcount(n) __builtin_popcountll(n)
+#define _(x) ((x) & (-(x)))
+#define cn cout << '\n';
+#define text "test"
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+//Const
+const int MOD = (int) 1e9 + 7;
+const int MOD2 = 1007681537;
+const int INF = (int) 1e9;
+const ll LINF = (ll) 1e18;
+const ld PI = acos((ld) -1);
+const ld EPS = 1e-9;
+
+//Function
+inline ll gcd(ll a, ll b) {ll r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline ll lcm(ll a, ll b) {return a / gcd(a, b) * b;}
+inline ll fpow(ll n, ll k, int p = MOD) {ll r = 1; for (; k; k >>= 1) {if (k & 1) r = r * n % p; n = n * n % p;} return r;}
+template<class T> inline int chkmin(T& a, const T& val) {return val < a ? a = val, 1 : 0;}
+template<class T> inline int chkmax(T& a, const T& val) {return a < val ? a = val, 1 : 0;}
+inline ll isqrt(ll k) {ll r = sqrt(k) + 1; while (r * r > k) r--; return r;}
+inline ll icbrt(ll k) {ll r = cbrt(k) + 1; while (r * r * r > k) r--; return r;}
+inline void addmod(int& a, int val, int p = MOD) {if ((a = (a + val)) >= p) a -= p;}
+inline void submod(int& a, int val, int p = MOD) {if ((a = (a - val)) < 0) a += p;}
+inline int mult(int a, int b, int p = MOD) {return (ll) a * b % p;}
+inline int inv(int a, int p = MOD) {return fpow(a, p - 2, p);}
+inline int sign(ld x) {return x < -EPS ? -1 : x > +EPS;}
+inline int sign(ld x, ld y) {return sign(x - y);}
+inline bool isUpcase(char x){ return x < 'a'; }
+inline char lowercase(char x){ if (isUpcase(x)) return char(x + 32);   return x; }
+inline char upcase(char x){ if (!isUpcase(x)) return char(x - 32);   return x; }
+//#define db(x) cerr << #x << " = " << (x) << " ";
+//#define endln cerr << "\n";
+
+// Direction
+const int dx[] = {0, -1, 0, 1, -1, -1, 1, 1};
+const int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
+
+const int maxn = 100 + 10;
+
+int n, m, dp[maxn][maxn][30][2];
+vii edge[maxn];
+
+int kp(int a, int b, int last, bool turn) {
+    int tmp = dp[a][b][last][turn];
+    if (tmp != -1) return tmp;
+    if (turn == 0) {
+        int ans = 0;
+        rep(i,0,sz(edge[a])) {
+            ii v = edge[a][i];
+            if (v.se >= last) ans |= kp(v.fi, b, v.se, 1);
+        }
+        return tmp = ans;
+    } else {
+        int ans = 1;
+        rep(i,0,sz(edge[b])) {
+            ii v = edge[b][i];
+            if (v.se >= last) ans &= kp(a, v.fi, v.se, 0);
+        }
+        return tmp = ans;
+    }
+}
+
+int main() {
+    //freopen(text".inp", "r", stdin);    freopen(text".out", "w", stdout);
+    ios_base::sync_with_stdio(0);   cin.tie(0); cout.tie(0);
+
+    ms(dp, -1);
+    cin >> n >> m;
+    FOR(i,1,m) {
+        int u, v;   char cahr;
+        cin >> u >> v >> cahr;
+        edge[u].pb( ii(v, cahr - 'a') );
+    }
+    FOR(i,1,n) {
+        FOR(j,1,n) if (kp(i, j, 0, 0))  cout << "A";
+            else cout << "B";
+        cout << "\n";
+    }
+
+    return 0;
+}
